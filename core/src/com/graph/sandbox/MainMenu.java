d966bdc526ade7479a0e8e86813d42050f135937
Destined132 <osmith04@btinternet.com>
2021-11-29 22:05:28 +0000

    fish3

--- core/src/com/graph/sandbox/MainMenu.java
+++ core/src/com/graph/sandbox/MainMenu.java
@@ -16,7 +16,9 @@ import com.badlogic.gdx.utils.Align;
 public class MainMenu implements Screen {
     private final Stage stage = new Stage();
     private final ShapeRenderer sr = new ShapeRenderer();
-    int settingsOpen = 0;
+    boolean settingsOpen = false;
+    boolean openGraphOpen = false;
+
 
 
     public MainMenu() {
@@ -25,9 +27,18 @@ public class MainMenu implements Screen {
 
 
 
-
         final Label errorText = new Label("*Sorry, this feature has not yet been implemented",buttonSkin, "error");
         errorText.setPosition(Gdx.graphics.getWidth()*(0.25f)+10, 5);
+
+        Label versionName = new Label("Oliver Smith | V1.0",buttonSkin, "peach");
+        versionName.setPosition(Gdx.graphics.getWidth()*(0.905f), 3);
+        stage.addActor(versionName);
+
+        Image title = new Image(new Texture(Gdx.files.internal("GTISTitle.png")));
+        title.setHeight(Gdx.graphics.getHeight()*(0.075f));
+        title.setWidth(Gdx.graphics.getHeight()*(0.075f)*(14));//
+        title.setPosition(Gdx.graphics.getWidth()*(0.27f), (Gdx.graphics.getHeight() * (0.92f)));
+        stage.addActor(title);
 
 
 
@@ -44,30 +55,69 @@ public class MainMenu implements Screen {
         settingsBox.setWidth(Gdx.graphics.getWidth() * (0.5f));
         settingsBox.setPosition((Gdx.graphics.getWidth() * (0.25f)), (Gdx.graphics.getHeight() * (0.25f)));*/
 
-        final Window settingsBox = new Window("Settings", buttonSkin, "maroon");
-        settingsBox.setHeight(Gdx.graphics.getHeight()*(2/3f));
+
+
+
+
+
+
+        final Window settingsBox = new Window("Settings", buttonSkin);
+        settingsBox.setHeight(Gdx.graphics.getHeight()*(0.6f));
         settingsBox.setWidth(Gdx.graphics.getWidth() * (0.5f));
-        settingsBox.setPosition((Gdx.graphics.getWidth() * (0.25f)), (Gdx.graphics.getHeight() * (0.25f)));
+        settingsBox.setPosition((Gdx.graphics.getWidth() * (0.35f)), (Gdx.graphics.getHeight() * (0.2f)));
         stage.addActor(settingsBox);
         settingsBox.setVisible(false);
         settingsBox.getTitleTable().align(Align.top|Align.right);
 
-
-        TextButton settingsClose = new TextButton("Close the settings here", buttonSkin,"maroonX");
+        TextButton settingsClose = new TextButton("X", buttonSkin,"orange-smallX");
         settingsClose.setHeight(Gdx.graphics.getHeight()*(1f));
         settingsClose.setWidth(Gdx.graphics.getWidth() * (1f));
         settingsClose.addListener (new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 settingsBox.setVisible(false);
-                settingsOpen = 0;
+                settingsOpen = false;
+                settingsBox.setPosition((Gdx.graphics.getWidth() * (0.3f)), (Gdx.graphics.getHeight() * (0.25f)));
 
             }
         });
         settingsBox.getTitleTable().add(settingsClose).height(Value.percentHeight(.05f, settingsBox)).width(Value.percentWidth(.05f, settingsBox));
-        //settingsClose.height(Value.percentWidth(.05F, settingsBox));
         settingsBox.align(Align.top|Align.right);
-        //settingsClose.setPosition(100,100);
+
+
+
+
+
+
+
+
+        final Window openGraphBox = new Window("Open Graph", buttonSkin,"maroon");
+        openGraphBox.setHeight(Gdx.graphics.getHeight()*(0.6f));
+        openGraphBox.setWidth(Gdx.graphics.getWidth() * (0.5f));
+        openGraphBox.setPosition((Gdx.graphics.getWidth() * (0.3f)), (Gdx.graphics.getHeight() * (0.25f)));
+        stage.addActor(openGraphBox);
+        openGraphBox.setVisible(false);
+        openGraphBox.getTitleTable().align(Align.top|Align.right);
+
+        TextButton openGraphClose = new TextButton("X", buttonSkin,"maroonX");
+        openGraphClose.setHeight(Gdx.graphics.getHeight()*(1f));
+        openGraphClose.setWidth(Gdx.graphics.getWidth() * (1f));
+        openGraphClose.addListener (new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                openGraphBox.setVisible(false);
+                openGraphOpen = false;
+                openGraphBox.setPosition((Gdx.graphics.getWidth() * (0.3f)), (Gdx.graphics.getHeight() * (0.25f)));
+
+            }
+        });
+        openGraphBox.getTitleTable().add(openGraphClose).height(Value.percentHeight(.05f, openGraphBox)).width(Value.percentWidth(.05f, openGraphBox));
+        openGraphBox.align(Align.top|Align.right);
+
+
+
+
+
 
 
 
@@ -95,7 +145,16 @@ public class MainMenu implements Screen {
         openGraph.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                stage.addActor(errorText);
+                if (openGraphOpen){
+                    openGraphOpen = false;
+                    openGraphBox.setVisible(false);
+                    openGraphBox.setPosition((Gdx.graphics.getWidth() * (0.3f)), (Gdx.graphics.getHeight() * (0.25f)));
+                }
+
+                else{
+                    openGraphOpen = true;
+                    openGraphBox.setVisible(true);
+                }
 
             }
         });
@@ -112,13 +171,14 @@ public class MainMenu implements Screen {
             @Override
             public void clicked(InputEvent event, float x, float y){
 
-                if (settingsOpen == 1){
-                    settingsOpen = 0;
+                if (settingsOpen){
+                    settingsOpen = false;
                     settingsBox.setVisible(false);
+                    settingsBox.setPosition((Gdx.graphics.getWidth() * (0.3f)), (Gdx.graphics.getHeight() * (0.25f)));
                 }
 
                 else{
-                    settingsOpen = 1;
+                    settingsOpen = true;
                     settingsBox.setVisible(true);
                 }
 
